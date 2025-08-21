c34dd5bbef7984738cb31b448303f83f7b74538e
0030482523032 <0030482523032@LTI6104.lti.fatecso.edu.br>
2025-08-21 20:14:57 -0300

    Exemplo 4

---
+++ prjExemplo4/src/Exemplo4.java
@@ -0,0 +1,12 @@
+public class Exemplo4 {
+    public static void main(String[] args) {
+        int i = 0;
+        int num = 7;
+        
+        while(i <= 10){
+            System.out.println(num + "*" + i + "=" + (num * i));
+            i++;
+        }
+    }
+    
+}
