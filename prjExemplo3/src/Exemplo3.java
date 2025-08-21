5bb7807434aa8bdfdfbd93ec51f529ce28c76801
Vinicius Teixeira <viniciusds.teixeira03@gmail.com>
2025-08-21 20:36:39 -0300

    Exemplo3

---
+++ prjExemplo3/src/Exemplo3.java
@@ -0,0 +1,14 @@
+public class Exemplo3 {
+
+    public static void main(String[] args) {
+        
+        int num = 7;
+        
+        System.out.println("\t\t\tTabuada do 7\n");
+        
+        for(int i = 0; i <= 10; i++){
+            System.out.println(num + "*" + i + "=" + (num * i));
+        }  
+    }
+    
+}
