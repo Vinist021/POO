d05b2fbaf2c5585dfc4787afa1aeb0e756e40744
VINICIUS DOS SANTOS TEIXEIRA <0030482523032@lti.fatecso.edu.br>
2025-08-26 21:07:19 -0300

    PrjExemplo1

---
+++ prjExemplo1/src/Exemplo1.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author Dimas
+ */
+public class Exemplo1 {
+    public static void main(String[] args) {
+       int idade;
+       double peso;
+       char sexo;
+       String nome;
+       
+       idade = 25;
+       peso = 78.5;
+       sexo = 'F';
+       nome = "Ana Beatriz";
+       
+       System.out.println("Nome: " + nome);
+       System.out.println("Sexo: " + sexo);
+       System.out.println("Idade: " + idade);
+       System.out.println("Peso: " + peso);
+    }    
+}
